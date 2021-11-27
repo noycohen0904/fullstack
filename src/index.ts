fa7d86ca230f25dcf5061adfb7226dbd32871ef1
noycohen0904 <noycohen0904@gmail.com>
2021-11-27 12:49:52 +0200

    apollo server setup

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,30 @@
 import { MikroORM } from "@mikro-orm/core";
-import { Post } from "./entities/post";
 import microConfig from "./mikro-orm.config";
+import express from "express";
+import { buildSchema } from "type-graphql";
+import { ApolloServer } from "apollo-server-express";
+import { HelloResolver } from "./resolvers/hello";
 
 const main = async () => {
   // Create and Run migration
   const orm = await MikroORM.init(microConfig);
   await orm.getMigrator().up();
 
-  const post = orm.em.create(Post, { title: "my first post" });
-  await orm.em.persistAndFlush(post);
+  // Create Server
+  const app = express();
+
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver],
+      validate: false,
+    }),
+  });
+
+  apolloServer.applyMiddleware({ app });
+
+  app.listen(4000, () => {
+    console.log("server up");
+  });
 };
 
 main().catch((error) => {
